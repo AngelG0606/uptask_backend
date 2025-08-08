d822fade0f034b8144b8831ebb010da75de2f56e
AngelGutierrez06Dev <angel.gutierrez.razo06@gmail.com>
2025-08-07 20:56:38 -0600

    Avances

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,16 @@ import { connectDB } from './config/db'
 import projectRouter from './routes/projectRoutes'
 import cors from 'cors'
 import { corsConfig } from './config/cors'
+import morgan from 'morgan'
 
 dotenv.config()
 
 connectDB()
 const app = express()
 app.use(cors(corsConfig))
+
+//logging
+app.use(morgan('dev'))
 
 app.use(express.json())
 
