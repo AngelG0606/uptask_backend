e1adbaec0ec4eee84879dfcd009fad00179b1ff4
AngelGutierrez06Dev <angel.gutierrez.razo06@gmail.com>
2025-08-13 13:53:06 -0600

    cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,17 +1,22 @@
-import { CorsOptions } from 'cors'
+import { CorsOptions } from 'cors';
 
-export const corsConfig : CorsOptions = {
-    origin : function (origin, callback) {
-        const whitelist = [process.env.FRONTEND_URL] 
-        
-        if(process.argv[2] === '--api') {
-            whitelist.push(undefined)
-        }
-        
-        if(whitelist.includes(origin)) {
-            callback(null, true)
-        } else {
-            callback(new Error('Error de CORS'))
-        }
+export const corsConfig: CorsOptions = {
+  origin: function (origin, callback) {
+    const whitelist = [process.env.FRONTEND_URL];
+
+    // Permitir sin origin (Postman, server-to-server, etc.)
+    if (!origin) {
+      return callback(null, true);
     }
-}
+
+    // Normalizar la URL (quitar barra final)
+    const normalizedOrigin = origin.replace(/\/$/, '');
+    const normalizedWhitelist = whitelist.map(url => url?.replace(/\/$/, ''));
+
+    if (normalizedWhitelist.includes(normalizedOrigin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Error de CORS'));
+    }
+  }
+};
