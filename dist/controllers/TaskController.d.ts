b4b90cb2a596ecad0fe2d25d8343cba51122cdd2
AngelGutierrez06Dev <angel.gutierrez.razo06@gmail.com>
2025-08-13 13:23:54 -0600

    deploy

--- dist/controllers/TaskController.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Request, Response } from 'express';
-export declare class TaskController {
-    static createTask: (req: Request, res: Response) => Promise<void>;
-    static getProjectTasks: (req: Request, res: Response) => Promise<void>;
-    static getTasksById: (req: Request, res: Response) => Promise<void>;
-    static updateTask: (req: Request, res: Response) => Promise<void>;
-    static deleteTask: (req: Request, res: Response) => Promise<void>;
-    static updateStatus: (req: Request, res: Response) => Promise<void>;
-}
